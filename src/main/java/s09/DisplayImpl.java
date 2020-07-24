201a588452bc48fd6a55ab92cc5bf706c79d8b2a
mutsune <dev@mutsune.com>
2020-07-24 17:32:33 +0900

    add s09

---
+++ src/main/java/s09/DisplayImpl.java
@@ -0,0 +1,10 @@
+package s09;
+
+// interface でも良さそう
+public abstract class DisplayImpl {
+    public abstract void rawOpen();
+
+    public abstract void rawPrint();
+
+    public abstract void rawClose();
+}
