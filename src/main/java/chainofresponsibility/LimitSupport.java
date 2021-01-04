75f9057d961fafa41659eba94af4e994e2c1f313
mutsune <dev@mutsune.com>
2021-01-04 19:20:28 +0900

    Chain of Responsibility code

---
+++ src/main/java/chainofresponsibility/LimitSupport.java
@@ -0,0 +1,17 @@
+package chainofresponsibility;
+
+public class LimitSupport extends Support {
+
+    private int limit;
+
+    public LimitSupport(String name, int limit) {
+        super(name);
+        this.limit = limit;
+    }
+
+    @Override
+    protected boolean resolve(Trouble trouble) {
+        return trouble.getNumber() < limit;
+    }
+
+}
